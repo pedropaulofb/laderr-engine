aa1b87775464edf523ae5c40362eb51f4e61806b
Pedro Paulo Favato Barcelos <pedropaulofb@gmail.com>
2025-02-07 16:57:27 +0100

    updated version

--- laderr_engine/laderr_lib/constants.py
+++ laderr_engine/laderr_lib/constants.py
@@ -1,6 +1,6 @@
 from rdflib import Namespace
 
 LADERR_NS = Namespace("https://w3id.org/pedropaulofb/laderr#")
-LADERR_SCHEMA_PATH = "C:\\Users\\FavatoBarcelosPP\\Dev\\laderr\\laderr-schema-v0.3.2.ttl"
+LADERR_SCHEMA_PATH = "C:\\Users\\FavatoBarcelosPP\\Dev\\laderr\\laderr-schema-v0.4.0.ttl"
 SHACL_FILES_PATH = "C:\\Users\\FavatoBarcelosPP\\Dev\\laderr\\shapes"
 
