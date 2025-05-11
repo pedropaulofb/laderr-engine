0250b0287427d85e7ae47ff478fef858a7258d2d
Pedro Paulo Favato Barcelos <pedropaulofb@gmail.com>
2025-05-12 00:43:56 +0200

    add new readme, minor fixes in code, add requirements.txt

---
+++ resources/exemple_exec_lib.py
@@ -0,0 +1,12 @@
+import sys
+
+from laderr_engine.laderr_lib import Laderr
+
+try:
+    Laderr.process_specification(
+        input_spec_path="C:\\Users\\<USERNAME>\\Dev\\laderr-engine\\laderr-engine\\example\\example_doc_in.toml",
+        output_file_base="C:\\Users\\<USERNAME>\\Dev\\laderr-engine\\laderr-engine\\example\\output_lib\\example_doc_out"
+    )
+except Exception as e:
+    print(f"Error processing LaDeRR specification: {e}")
+    sys.exit(1)
