380b2a03b6d02697c4b08cd42c960c6dadb1fe4b
Pedro Paulo Favato Barcelos <pedropaulofb@gmail.com>
2025-06-16 21:55:27 +0200

    refactor(examples): restructure and rename scenario files for clarity
    - Added illustrative PNGs for initial, vulnerable, and resilient scenarios
    - Renamed scenario1 and scenario2 TOML/TTL files to `example_scenario_vulnerable` and `example_scenario_resilient`
    - Refactored folder structure under `output_scenario_vulnerable/` and `output_scenario_resilient/`
    - Removed outdated output images from previous `output_scenario1/` and `output_scenario2/`
    - Updated content in resilient scenario TOML and TTL files (e.g., RW7 replacing RO8)

--- resources/exemple_exec_lib.py
+++ resources/exemple_exec_lib.py
@@ -4,8 +4,8 @@ from laderr_engine.laderr_lib import Laderr
 
 try:
     Laderr.process_specification(
-        input_spec_path="C:\\Users\\<USERNAME>\\Dev\\laderr-engine\\laderr-engine\\example\\example_doc_in.toml",
-        output_file_base="C:\\Users\\<USERNAME>\\Dev\\laderr-engine\\laderr-engine\\example\\output_lib\\example_doc_out"
+        input_spec_path="C:\\Users\\PedroPauloFavatoBarc\\Dev\\laderr-engine\\example\\edoc_examples\\example_scenario_resilient.toml",
+        output_file_base="C:\\Users\\PedroPauloFavatoBarc\\Dev\\laderr-engine\\example\\edoc_examples\\output_scenario_resilient\\out_scenario_resilient",
     )
 except Exception as e:
     print(f"Error processing LaDeRR specification: {e}")
